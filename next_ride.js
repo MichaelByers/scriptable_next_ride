f877c4457920eea98d6dd0db107d9e0a1696e59d
MichaelByers <michael.byers@comcast.net>
2025-07-01 14:32:34 -0600

    Add files via upload

---
+++ next_ride.js
@@ -0,0 +1,123 @@
+const w = new ListWidget();
+w.backgroundColor = new Color("#f7f7f7");
+const myGradient = new LinearGradient();
+myGradient.colors = [new Color("#0000ff"), new Color("#f7f7f7")];
+myGradient.locations = [0,0.35];
+w.backgroundGradient = myGradient;
+// RTD route info
+let config = {
+    route:    25990,
+    direction: 'Southbound',
+    branch: 'E'
+};
+// setup display
+let stack = w.addStack();
+stack.centerAlignContent();
+const title = stack.addText('RTD Next Ride');
+title.textColor = Color.white();
+title.font = Font.boldSystemFont(18);
+stack = w.addStack();
+const spacer = stack.addText(' ');
+// get data
+const tUrl = 'https://nodejs-prod.rtd-denver.com/api/v2/nextride/stops/' + config.route;
+console.log("Getting RTD route info");
+let route = await get({url: tUrl});
+// calculate table
+let timeTable = [];
+let max = 0;
+const stopName = route.name;
+console.log('sotpName: ', stopName);
+//fill time table array
+route.branches.map((branch) => {
+    if(branch.directionName === config.direction && branch.id === config.branch) {
+        branch.upcomingTrips.map((trip) => {
+            let times = {
+                "sTime": trip.scheduledArrivalTime,
+                "pTime": trip.predictedArrivalTime,
+                "status": trip.tripStopStatus
+            }
+            timeTable.push(times);
+        });
+    }
+});
+//sort time table
+timeTable.sort(function(a,b){
+    return parseFloat(a.sTime) - parseFloat(b.sTime);
+});
+max = timeTable.length;
+//only show next 3
+if(max > 3) {
+    max = 3;
+}
+//Name
+stack = w.addStack();
+stack.centerAlignContent();
+stack.addText(stopName);
+//Trips
+for(let i=0; i<max; i++) {
+    let df = new DateFormatter();
+    df.dateFormat = 'h:mm';
+    let sTime = df.string(new Date(timeTable[i].sTime));
+    let pTime = null;
+    let dTime = null;
+    let dTimeStr = "On Time";
+    stack = w.addStack();
+    stack.centerAlignContent();
+
+    if(timeTable[i].status === "CANCELLED") {
+        let status = stack.addText(timeTable[i].status);
+        status.textColor = Color.red();
+    } else {
+        stack.addText(sTime);
+        if(timeTable[i].pTime){
+            pTime = df.string(new Date(timeTable[i].pTime));
+            if(timeTable[i].pTime == timeTable[i].sTime){
+                dTimeStr = 'On Time'
+                let label = stack.addText(dTimeStr);
+                label.textColor = Color.green();
+            } else if(timeTable[i].pTime > timeTable[i].sTime){
+                dTime = Math.trunc((timeTable[i].pTime - timeTable[i].sTime)/60000);
+                dTimeStr = ":"+ dTime + " late";
+                let label = stack.addText(dTimeStr);
+                label.textColor = Color.red();
+            } else {
+                dTime = Math.trunc((timeTable[i].sTime - timeTable[i].pTime)/60000);
+                dTimeStr = ":"+ dTime + " early";
+                let label = stack.addText(dTimeStr);
+                label.textColor = Color.green();
+            }
+        }
+    }
+    stack.addSpacer(10);
+}
+
+// display
+Script.setWidget(w);
+w.presentSmall();
+Script.complete();
+
+async function get(opts){
+    try {
+        const request = new Request(opts.url);
+        request.headers = {
+            ...opts.headers,
+            ...this.defaultHeaders
+        }
+        let result = await request.loadJSON();
+        console.log("RESULT: ", result);
+        if(result.errors === undefined){
+            return result;
+        } else {
+            let alert = new Alert();
+            alert.title = "ERROR";
+            alert.message = result.errors[0].detail;
+            alert.presentAlert();
+            return {};
+        }
+    } catch (error) {
+        console.log('Could not fetch data: ' + error);
+        return {directions: {travelTimeInSeconds: 0}};
+    }
+}
+
+
